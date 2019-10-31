c7ee9e830c36e9b50888236afed8322de6907841
Nicolas Schejtman <nschejtman93@gmail.com>
2019-10-31 12:01:51 -0300

    Improve flattened emission queue performance

--- shared/src/main/scala/amf/plugins/document/graph/emitter/flattened/utils/EmissionQueue.scala
+++ shared/src/main/scala/amf/plugins/document/graph/emitter/flattened/utils/EmissionQueue.scala
@@ -1,5 +1,6 @@
 package amf.plugins.document.graph.emitter.flattened.utils
 
+import org.mulesoft.lexer.Queue
 import org.yaml.builder.DocBuilder.Part
 
 import scala.collection.mutable
@@ -19,26 +20,26 @@ trait Metadata {
   */
 case class EmissionQueue[T]() {
 
-  private val queue: mutable.Queue[Emission[T] with Metadata] = mutable.Queue.empty
-
-  val pastIds: mutable.HashSet[String] = mutable.HashSet.empty
+  private val queue: Queue[Emission[T] with Metadata] = new Queue()
+  // Ids in queue or previously in queue
+  private val knownIds: mutable.HashSet[String] = mutable.HashSet[String]()
 
   def tryEnqueue(e: Emission[T] with Metadata): Try[Unit] = {
     if (accepts(e)) {
-      queue.enqueue(e)
+      queue += e
+      e.id.map { id =>
+        knownIds += id
+      }
       Success()
     } else {
       Failure(new IllegalArgumentException("Element already emitted"))
     }
   }
 
-  def hasPendingEmissions: Boolean = queue.nonEmpty
+  def hasPendingEmissions: Boolean = !queue.isEmpty
 
   def nextEmission(): Emission[T] with Metadata = {
-    val next = queue.dequeue()
-    next.id.map { id =>
-      pastIds += id
-    }
+    val next = queue.dequeue
     next
   }
 
@@ -48,12 +49,6 @@ case class EmissionQueue[T]() {
     * @param e emission
     * @return
     */
-  def accepts(e: Emission[T] with Metadata): Boolean = {
-    val inQueue = queue.contains(e)
-    val previouslyInQueue = e.id.exists { id =>
-      pastIds.contains(id)
-    }
-    !inQueue && !previouslyInQueue
-  }
+  def accepts(e: Emission[T] with Metadata): Boolean = !e.id.exists { id => knownIds.contains(id) }
 
 }

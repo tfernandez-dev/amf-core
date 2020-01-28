6526d9d195da57b9336457cc504fc8d3a627c7b3
Agustin Bettati <bettatiagustin@gmail.com>
2020-01-28 15:13:16 -0300

    parameters to define max yaml references

--- shared/src/main/scala/amf/core/client/ParsingOptions.scala
+++ shared/src/main/scala/amf/core/client/ParsingOptions.scala
@@ -7,8 +7,9 @@ import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
 @JSExportAll
 @JSExportTopLevel("parser.ParsingOptions")
 class ParsingOptions {
-  private var amfJsonLdSerialization      = true
-  private var baseUnitUrl: Option[String] = None
+  private var amfJsonLdSerialization          = true
+  private var baseUnitUrl: Option[String]     = None
+  private var maxYamlReferences: Option[Long] = None
 
   /**
     * Parse specific AMF JSON-LD serialization
@@ -38,9 +39,17 @@ class ParsingOptions {
     this
   }
 
+  /**
+    * Defines an upper bound of yaml alias that will be resolved when parsing a DataNode
+    */
+  def setMaxYamlReferences(value: Long): ParsingOptions = {
+    maxYamlReferences = Some(value)
+    this
+  }
 
   def isAmfJsonLdSerilization: Boolean = amfJsonLdSerialization
   def definedBaseUrl: Option[String]   = baseUnitUrl
+  def getMaxYamlReferences: Option[Long]  = maxYamlReferences
 }
 
 object ParsingOptions {
--- shared/src/main/scala/amf/internal/environment/Environment.scala
+++ shared/src/main/scala/amf/internal/environment/Environment.scala
@@ -4,16 +4,17 @@ import amf.core.unsafe.PlatformSecrets
 import amf.internal.reference.ReferenceResolver
 import amf.internal.resource.ResourceLoader
 
-case class Environment(loaders: Seq[ResourceLoader], resolver: Option[ReferenceResolver]) {
-  def add(loader: ResourceLoader): Environment  = Environment(loader +: loaders, resolver)
-  def withLoaders(loaders: Seq[ResourceLoader]) = Environment(loaders, resolver)
-  def withResolver(resolver: ReferenceResolver) = Environment(loaders, Some(resolver))
+case class Environment(loaders: Seq[ResourceLoader], resolver: Option[ReferenceResolver], maxYamlReferences: Option[Long]) {
+  def add(loader: ResourceLoader): Environment  = Environment(loader +: loaders, resolver, maxYamlReferences)
+  def withLoaders(loaders: Seq[ResourceLoader]) = Environment(loaders, resolver, maxYamlReferences)
+  def withResolver(resolver: ReferenceResolver) = Environment(loaders, Some(resolver), maxYamlReferences)
+  def setMaxYamlReferences(value: Long) = Environment(loaders, resolver, Some(value))
 }
 
 object Environment extends PlatformSecrets {
-  def apply(): Environment                             = new Environment(platform.loaders(), None)
-  def apply(resolver: ReferenceResolver): Environment  = new Environment(Nil, Some(resolver))
-  def apply(loaders: Seq[ResourceLoader]): Environment = new Environment(loaders, None)
+  def apply(): Environment                             = new Environment(platform.loaders(), None, None)
+  def apply(resolver: ReferenceResolver): Environment  = new Environment(Nil, Some(resolver), None)
+  def apply(loaders: Seq[ResourceLoader]): Environment = new Environment(loaders, None, None)
   def apply(loader: ResourceLoader): Environment       = Environment.apply(Seq(loader))
-  def empty(): Environment                             = new Environment(Nil, None)
+  def empty(): Environment                             = new Environment(Nil, None, None)
 }

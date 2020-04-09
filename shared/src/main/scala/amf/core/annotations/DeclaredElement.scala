24958eb6128541aa71125626b94c2c9834910fe4
Hernan Najles <hernan.naj+github@gmail.com>
2020-04-09 14:54:51 -0300

    Use always meta for new instances at clone. Overridee meta for local implementation wheen need to override new instance

--- shared/src/main/scala/amf/core/annotations/DeclaredElement.scala
+++ shared/src/main/scala/amf/core/annotations/DeclaredElement.scala
@@ -1,6 +1,9 @@
 package amf.core.annotations
 
+import amf.core.metamodel.domain.{DomainElementModel, ModelDoc}
+import amf.core.metamodel.{Field, ModelDefaultBuilder, Obj}
 import amf.core.model.domain._
+import amf.core.vocabulary.ValueType
 
 case class DeclaredElement() extends SerializableAnnotation with PerpetualAnnotation {
   override val name: String = "declared-element"
@@ -17,5 +20,16 @@ trait ErrorDeclaration extends DomainElement {
 
   override def withId(value: String): ErrorDeclaration.this.type = super.withId(namespace + value)
 
-  def newErrorInstance:ErrorDeclaration
+  // Temp hack to avoid change the model. If you serialize the model and parsed it again the mark of error will be lost
+  override def meta: Obj = new DomainElementModel {
+    override def modelInstance: AmfObject = newErrorInstance
+
+    override def fields: List[Field] = originalMeta.fields
+    override val `type`: List[ValueType] = originalMeta.`type`
+    override val doc:ModelDoc = originalMeta.doc
+
+  }
+
+  protected def newErrorInstance:ErrorDeclaration
+  protected def originalMeta:Obj // need to force implementation of the error instance to difference between the normal element and those which are marked as error
 }

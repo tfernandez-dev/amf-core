eb3c5309afb132209cf6b21c01201957fda7fabb
Nicolas Schejtman <nschejtman93@gmail.com>
2020-04-16 14:43:03 -0300

    Refactor: Extract amf.core.parser.Reference to separate file

---
+++ shared/src/main/scala/amf/core/parser/ReferenceUtils.scala
@@ -0,0 +1,62 @@
+package amf.core.parser
+
+import amf.core.model.document._
+import amf.core.remote.File.FILE_PROTOCOL
+import amf.core.remote.HttpParts.{HTTPS_PROTOCOL, HTTP_PROTOCOL}
+import amf.core.utils.AmfStrings
+import org.yaml.model.YNode
+
+import scala.collection.mutable
+
+case class ReferenceResolutionResult(exception: Option[Throwable], unit: Option[BaseUnit])
+
+case class RefContainer(linkType: ReferenceKind, node: YNode, fragment: Option[String])
+
+case class ReferenceCollector() {
+  private val refs = mutable.Map[String, Reference]()
+
+  def +=(key: String, kind: ReferenceKind, node: YNode): Unit = {
+    val (url, fragment) = ReferenceFragmentPartition(key)
+    refs.get(url) match {
+      case Some(reference: Reference) => refs.update(url, reference + (kind, node, fragment))
+      case None                       => refs += url -> Reference(url, kind, node, fragment)
+    }
+  }
+
+  def toReferences: Seq[Reference] = refs.values.toSeq
+}
+
+object EmptyReferenceCollector extends ReferenceCollector {}
+
+/**
+  * Splits references between their base url and local path
+  * E.g. https://some.path.json#/local/path -> ("https://some.path.json", "local/path")
+  */
+object ReferenceFragmentPartition {
+  // Is it always a URL? If we can have local references then it is not a URL
+  def apply(url: String): (String, Option[String]) = {
+    if (isExternalReference(url)) {
+      url.split("#") match { // how can i know if the # its part of the uri or not? uri not valid???
+        case Array(basePath) if basePath.endsWith("#") => (basePath.substring(0, basePath.length - 2), None)
+        case Array(basePath)                           => (basePath, None)
+        case Array(basePath, localPath)                => (basePath, Some(localPath))
+        case other                                     =>
+          //  -1 of the length diff and -1 for # char
+          val str = url.substring(0, url.length - 1 - other.last.length)
+          (str, Some(other.last))
+      }
+    }
+    else (url, None)
+  }
+
+  /**
+    * Checks if reference corresponds to a remote resource
+    * @param referenceLocator like an URL but not uniform since we can have local references
+    * @return
+    */
+  private def isExternalReference(referenceLocator: String) = {
+    (referenceLocator.normalizeUrl.startsWith(FILE_PROTOCOL) || referenceLocator.startsWith(HTTPS_PROTOCOL) || referenceLocator
+      .startsWith(HTTP_PROTOCOL)) && !referenceLocator
+      .startsWith("#")
+  }
+}

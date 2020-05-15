961eb55bdf479632a47aa1d90dee8a86c91a1909
Tomas Fernandez <tfernandez@mulesoft.com>
2020-05-15 11:37:31 -0300

    Refactor: made DynamicLiteralParser static
    (cherry picked from commit a5f93ac3661e66a88628bc8320adb8106f561215)

--- shared/src/main/scala/amf/core/rdf/parsers/DynamicLiteralParser.scala
+++ shared/src/main/scala/amf/core/rdf/parsers/DynamicLiteralParser.scala
@@ -3,7 +3,7 @@ package amf.core.rdf.parsers
 import amf.core.model.domain.ScalarNode
 import amf.core.rdf.Literal
 
-class DynamicLiteralParser {
+object DynamicLiteralParser {
   def parse(l: Literal): ScalarNode = {
     val result = ScalarNode()
     l.literalType.foreach(t => result.withDataType(t))

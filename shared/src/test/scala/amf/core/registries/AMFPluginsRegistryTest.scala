acc5d807dc1a59c0289d8e4d7d228aecf36a9b21
Hernan Najles <hernan.naj+github@gmail.com>
2020-06-22 20:33:09 -0300

    Add method for unregister domain plugin from static registry

--- shared/src/test/scala/amf/core/registries/AMFPluginsRegistryTest.scala
+++ shared/src/test/scala/amf/core/registries/AMFPluginsRegistryTest.scala
@@ -1,3 +1,25 @@
-package amf.core.registries class AMFPluginsRegistryTest {
+package amf.core.registries
 
+import amf.plugins.document.graph.AMFGraphPlugin
+import org.scalatest.FunSuite
+
+class AMFPluginsRegistryTest extends FunSuite{
+
+
+  test("test register and unregister document plugin"){
+
+    AMFPluginsRegistry.registerDocumentPlugin(AMFGraphPlugin)
+
+    assert(AMFPluginsRegistry.documentPluginForID(AMFGraphPlugin.ID).isDefined)
+
+    assert(AMFPluginsRegistry.documentPluginForMediaType(AMFGraphPlugin.documentSyntaxes.head).nonEmpty)
+    assert(AMFPluginsRegistry.documentPluginForVendor(AMFGraphPlugin.vendors.head).nonEmpty)
+
+    AMFPluginsRegistry.unregisterDocumentPlugin(AMFGraphPlugin)
+
+    assert(AMFPluginsRegistry.documentPluginForID(AMFGraphPlugin.ID).isEmpty)
+
+    assert(AMFPluginsRegistry.documentPluginForMediaType(AMFGraphPlugin.documentSyntaxes.head).isEmpty)
+    assert(AMFPluginsRegistry.documentPluginForVendor(AMFGraphPlugin.vendors.head).isEmpty)
+  }
 }
